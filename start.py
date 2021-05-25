72709adffdd7b56ed3a8720df8b7d6b875fcafd6
AR1905 <assaf.reiner@gmail.com>
2021-05-26 00:11:53 +0300

    plot_r added to main

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 import relaxation_model
 
-model = relaxation_model.RelaxationModel(8, 14, 0.1, 0.1)
+model = relaxation_model.RelaxationModel(8, 14, 0.05, 0.1)
 model.initDatabase()
 matrix = model.space._convert_space_into_matrix()
 model.relax("result.pickle")
