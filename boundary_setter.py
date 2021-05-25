b5fbd6175f047fe3b94af748880917c3b78a6c3c
shimonuri <54213090+shimonuri@users.noreply.github.com>
2021-05-25 23:04:15 +0300

    Update boundary_setter.py

--- boundary_setter.py
+++ boundary_setter.py
@@ -19,6 +19,7 @@ FAR_POTENTIAL = 5
 def set_boundary(current_space: space.Space):
     _set_ball_boundary(current_space)
     _set_far_boundary(current_space)
+    _set_z_zero_boundary(current_space)
 
 
 def _set_ball_boundary(current_space):
@@ -39,7 +40,12 @@ def _set_far_boundary(current_space):
             r, current_space.zmax, BALL.potential, is_changeable=False
         )
 
-    for z in np.arange(0, current_space.zmax + 0.1, 0.1):
+    for z in np.arange(0.1, current_space.zmax, 0.1):
         current_space.set_point(
             current_space.rmax, z, BALL.potential, is_changeable=False
         )
+
+
+def _set_z_zero_boundary(current_space):
+    for r in np.arange(0, current_space.rmax + 0.1, 0.1):
+        current_space.set_point(r, 0, 0, is_changeable=False)
