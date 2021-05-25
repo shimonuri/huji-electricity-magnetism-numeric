315f8c776662e6c5e9dd83a3dbe9886ce537ec4a
shimonuri <54213090+shimonuri@users.noreply.github.com>
2021-05-25 23:48:50 +0300

    Updating code

--- boundary_setter.py
+++ boundary_setter.py
@@ -7,7 +7,7 @@ import space
 class Ball:
     r: int
     z: int
-    potential: int
+    potential: float
     radius: int
 
 
@@ -18,34 +18,33 @@ FAR_POTENTIAL = 5
 
 def set_boundary(current_space: space.Space):
     _set_ball_boundary(current_space)
-    _set_far_boundary(current_space)
     _set_z_zero_boundary(current_space)
 
 
 def _set_ball_boundary(current_space):
-    for r in np.arange(BALL.r, BALL.r + BALL.radius + 0.1, 0.1):
-        current_radius = BALL.radius * np.sqrt(1 - r / BALL.radius)
-        for z in np.arange(BALL.z - current_radius, BALL.z + current_radius + 0.1, 0.1):
+    for r, z in current_space.get_changeable():
+        distance_from_ball = np.sqrt((z - 4) ** 2 + r ** 2)
+        if distance_from_ball <= 1:
             current_space.set_point(
-                r=np.round(r, decimals=1),
-                z=np.round(z, decimals=1),
+                r=np.round(r, decimals=current_space.decimals),
+                z=np.round(z, decimals=current_space.decimals),
                 val=BALL.potential,
                 is_changeable=False,
             )
 
 
 def _set_far_boundary(current_space):
-    for r in np.arange(0, current_space.rmax + 0.1, 0.1):
+    for r in np.arange(0, current_space.rmax + current_space.h, current_space.h):
         current_space.set_point(
             r, current_space.zmax, BALL.potential, is_changeable=False
         )
 
-    for z in np.arange(0.1, current_space.zmax, 0.1):
+    for z in np.arange(current_space.h, current_space.zmax, current_space.h):
         current_space.set_point(
             current_space.rmax, z, BALL.potential, is_changeable=False
         )
 
 
 def _set_z_zero_boundary(current_space):
-    for r in np.arange(0, current_space.rmax + 0.1, 0.1):
+    for r in np.arange(0, current_space.rmax + current_space.h, current_space.h):
         current_space.set_point(r, 0, 0, is_changeable=False)
