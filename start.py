70fbf58697e85ab5d83dfcb552bc98443b9fa70c
shimonuri <54213090+shimonuri@users.noreply.github.com>
2021-05-26 10:36:01 +0300

    adding two balls

--- start.py
+++ start.py
@@ -2,5 +2,4 @@ import relaxation_model
 
 model = relaxation_model.RelaxationModel(8, 14, 0.1, 0.1)
 model.initDatabase()
-matrix = model.space._convert_space_into_matrix()
 model.relax("result.pickle")
