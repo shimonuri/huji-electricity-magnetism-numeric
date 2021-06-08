0c7db1169a2241858551e3a90db331665bf1e3a8
AR1905 <assaf.reiner@gmail.com>
2021-06-09 00:38:22 +0300

    function names

--- analytical_sol.py
+++ analytical_sol.py
@@ -3,36 +3,36 @@ R = 2
 a = 8
 accuracy = 0.1/100
 
-def d(n):
+def get_d(n):
     if n == 0:
         return 0
     else:
-        return R ** 2 / (a - d(n-1))
+        return R ** 2 / (a - get_d(n-1))
 
-def q(n):
+def get_q(n):
     if n == 0:
         return V0 * R
     else:
-        return q(n-1) * R / (a - d(n-1))
+        return get_q(n-1) * R / (a - get_d(n-1))
 
 
-def V(n, z):
+def get_V(n, z):
     if z < R:
         return V0
     else:
-        return q(n) / abs(z + d(n) - a/2) - q(n)/ abs(z - d(n) + a/2)
+        return get_q(n) / abs(z + get_d(n) - a/2) - get_q(n)/ abs(z - get_d(n) + a/2)
 
-def Vtot(n, z):
+def calc_Vtot(n, z):
     voltage = 0
     for i in range(1, n+1):
-        voltage += V(i, z)
+        voltage += get_V(i, z)
     return voltage
 
 def calc_iter():
     i = 1
     iters = []
     while True:
-        iters.append(Vtot(i, R))
+        iters.append(calc_Vtot(i, R))
         if i >= 2:
             err = abs(iters[i-1] - iters[i-2]) / abs(iters[i-2])
             if err < accuracy:
