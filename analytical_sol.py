24769aa3f89f9643ac99e7f9643029dbfc82587f
AR1905 <assaf.reiner@gmail.com>
2021-06-09 00:57:13 +0300

    plot added

--- analytical_sol.py
+++ analytical_sol.py
@@ -1,13 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 V0 = 10
 R = 2
 a = 8
+h = 0.1
 accuracy = 0.1/100
+N = 7
+
 
 def get_d(n):
     if n == 0:
         return 0
     else:
         return R ** 2 / (a - get_d(n-1))
+
 
 def get_q(n):
     if n == 0:
@@ -17,16 +24,18 @@ def get_q(n):
 
 
 def get_V(n, z):
-    if z < R:
+    if abs(z - a/2) < R:
         return V0
     else:
         return get_q(n) / abs(z + get_d(n) - a/2) - get_q(n)/ abs(z - get_d(n) + a/2)
+
 
 def calc_Vtot(n, z):
     voltage = 0
     for i in range(1, n+1):
         voltage += get_V(i, z)
     return voltage
+
 
 def calc_iter():
     i = 1
@@ -39,8 +48,25 @@ def calc_iter():
                 return i
         i += 1
 
+def calc_V_r0():
+    potential_vals = []
+    domain = np.arange(0, 14 + h, h)
+    for z in domain:
+        potential_vals.append(calc_Vtot(N, z))
+    plot_r(domain, potential_vals)
+
+
+
+def plot_r(z_vals, potential_vals):
+    plt.plot(z_vals, potential_vals)
+    plt.xlabel("z")
+    plt.ylabel("Potential")
+    plt.show()
+
+
 if __name__ == '__main__':
     print(calc_iter())
+    calc_V_r0()
 
 
 
