1975078d9e5cb70746d7edfb96d2b7b2625b3cf0
shimonuri <54213090+shimonuri@users.noreply.github.com>
2021-06-09 00:58:49 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 import relaxation_model
 
-model = relaxation_model.RelaxationModel(8, 14, 0.1, 0.1)
+model = relaxation_model.RelaxationModel(8, 14, 0.2, 0.1)
 model.initDatabase()
 model.relax("result.pickle")
